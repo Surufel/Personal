ae01236a6aee9dd13fd0e86963b202073a7bd13f
Sif <sifer.aseph@nyu.edu>
2019-07-30 18:04:12 -0400

    Modernize

--- 1.pysifer/Out of Date/hello world.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# Sifer Aseph
-# Prints a "hello cruel, cruel world" statement.
-
-print "hello world"
---
+++ 1.pysifer/Out of Date/hello_world.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Sifer Aseph
+"""Prints a "hello world" statement."""
+
+def main():
+    """Utterly standard."""
+    print("Hello cruel world.")
+
+if __name__ == "__main__":
+    main()
