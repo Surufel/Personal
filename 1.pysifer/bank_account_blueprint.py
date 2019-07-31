5fae5b7c1ffec4886ac2c1c067174e130dd9e27f
Sif <sifer.aseph@nyu.edu>
2019-07-30 21:07:11 -0400

    Update

--- 1.pysifer/bank_account_blueprint.py
+++ 1.pysifer/bank_account_blueprint.py
@@ -1,22 +1,33 @@
 #!/usr/bin/env python3
 # Sifer Aseph
 """
-classes
+Bank account blueprint.
+
 https://wiki.python.org/moin/SimplePrograms
 """
 
-class BankAccount(object):
+class BankAccount():
+    """Bank account object."""
+
     def __init__(self, initial_balance=0):
+        """Initialize starting balance."""
         self.balance = initial_balance
+
     def deposit(self, amount):
+        """Deposit into bank account."""
         self.balance += amount
+
     def withdraw(self, amount):
+        """Withdraw from bank account."""
         self.balance -= amount
+
     def overdrawn(self):
+        """Show an overdrawn account."""
         return self.balance < 0
 
 def main():
-    my_account = BankAccount(15)
+    """Test run."""
+    my_account = BankAccount(150000)
     my_account.withdraw(5)
     print(my_account.balance)
 
