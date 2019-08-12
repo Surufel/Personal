e438be3871094005cb3306a7963be3d23e34e627
Sif <sifer.aseph@nyu.edu>
2019-08-11 22:41:52 -0400

    Empathy

---
+++ 1.pysifer/Older/a_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Sifer Aseph
+"""Line 6 creates a list where every element is the same sublist."""
+
+n = 10
+names = [[]] * n
+
+print(names)
+
+me = "sif"
+m = 5
+names[m].append(me)
+
+print(names)
