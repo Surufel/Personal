df36c7edf35635dab6db8d7b739777331f557af7
Sif <sifer.aseph@nyu.edu>
2019-08-11 22:01:12 -0400

    Compassion

---
+++ 1.pysifer/Older/pre-school.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# Sifer Aseph
+"""..."""
+
+def main(): 0 1 2 3 4 5 6 7 8 9
+    zero = 1
+    one = 0
+    two = 0
+    three = 0
+    four = 1
+    five = 0
+    six = 1
+    seven = 0
+    eight = 2
+    nine = 1
+
+if __name__ == "__main__":
+    main()
