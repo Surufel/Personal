781c01f623c7eb4f2798baa93d8701b6c4293b0f
Sif <sifer.aseph@nyu.edu>
2019-08-12 03:20:28 -0400

    Kindness

---
+++ 1.pysifer/Older/zip.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Sifer Aseph
+
+category1 = ["A", "B", "C"]
+category2 = [1, 2, 3]
+category3 = ["sif", "aseph"]
+
+new_list = list(zip(category1, category2, category3))
+print(new_list)
